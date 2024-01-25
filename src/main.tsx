7b27b283ea0b605fca47757519514509ccbb65aa
felipeqq <uluizfelipe@gmail.com>
2024-01-25 20:11:54 -0300

    fix: :wrench: Add react-toastify for displaying toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from 'react-query';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { queryClient } from './config/lib/react-query/query-client.ts';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const container = document.getElementById('root');
+const root = createRoot(container as Element);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
