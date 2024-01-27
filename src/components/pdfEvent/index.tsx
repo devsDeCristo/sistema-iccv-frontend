7e464939a9da1683eac448fc1d0f289987dcfe67
maressaD <maressadld@gmail.com>
2024-01-27 18:40:39 -0300

    novo modelo de pdf

--- src/components/pdfEvent/index.tsx
+++ src/components/pdfEvent/index.tsx
@@ -1,138 +1,180 @@
 //import React from "react";
-import { Page, Text, View, Document, Font, StyleSheet, Image, } from '@react-pdf/renderer';
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  Font,
+  StyleSheet,
+  Image,
+} from '@react-pdf/renderer';
 interface PdfProps {
   data: object[];
+  textFooter: string;
 }
-import logoIccv from "../../assets/ic-logo.png";
-import logoCursilho from "../../assets/logo-cursilho.jpeg";
+interface UserRectangleProps {
+  user: object;
+}
+interface HeaderProps {}
+interface FooterProps {
+  text: string;
+}
+import logoIccv from '../../assets/logo-iccv.png';
+import logoEvento from '../../assets/4-curs-fem.png';
+import logoCursilho from '../../assets/logo-cursilho.png';
 const styles = StyleSheet.create({
   body: {
-   paddingTop: 35,
-   paddingBottom: 65,
-   paddingHorizontal: 35,
- },
+    paddingTop: 35,
+    paddingBottom: 65,
+    paddingHorizontal: 35,
+  },
   page: {
-   fontFamily: "Times-Roman",
-   padding: 40,
- },
- header: {
-  gap:"10px",
-   fontSize: 12,
-   marginBottom: 20,
-   textAlign: 'center',
-   alignItems:"center",
-   justifyContent:"center",
-   flexDirection: "row",
-   color: 'grey',
-   marginTop: 10,
- },
- section: {
-   fontFamily: "Times-Roman",
-   marginBottom: 10,
- },
- title: {
-   fontSize: 24,
-   textAlign: 'center',
-   fontFamily: 'Oswald',
-   marginBottom: 10,
- },
- text: {
-   fontFamily: "Times-Roman",
-   fontSize: 12,
-   marginBottom: 5,
- },
- subtitle: {
-   fontFamily: "Times-Roman",
-   fontSize: 10,
-   marginBottom: 5,
- },
- table: {
-   fontFamily: "Times-Roman",
-  // display: "table",
-   width: "auto",
-   marginBottom: 10,
- },
- tableRow: {
-   fontFamily: "Times-Roman",
-   flexDirection: "row",
- },
- tableCell: {
-   fontFamily: "Times-Roman",
-   width: "25%",
-   borderWidth: 1,
-   borderColor: "#000",
-   padding: 5,
-   fontSize: 11,
- },
- cell: {
-   fontFamily: "Times-Roman",
-   width: "30%",
-   borderWidth: 1,
-   borderColor: "#000",
-   padding: 5,
-   fontSize: 11,
-   flexDirection: "row",
- },
- image: { width: 70 , height: 70},
- imageEvent: { width: 70 , height: 70,borderRadius:45},
- divider: {
-   marginTop: "10px",
-   borderTop: "2px dashed #bbb",
-   marginBottom: "20px",
- },
- footer: {
-   position: "absolute",
-   fontSize: 12,
-   bottom: 30,
-   left: 40,
-   right: 0,
-   color: "grey",
- },
- footerText: {
-   fontFamily: "Times-Roman",
-   fontSize: 10,
- },
- tabelaTitle: { fontFamily: "Times-Bold", fontSize: 12 },
+    padding: 40,
+  },
+  header: {
+    gap: '10px',
+    fontSize: 12,
+    marginBottom: 20,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    backgroundColor: '#f8d2f8',
+    //color: 'grey',
+    paddingHorizontal: '20px',
+    borderRadius: '10px',
+    //marginTop: 10,
+  },
+  footer: {
+    gap: '5px',
+    fontSize: 12,
+    marginBottom: 20,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#f8d2f8',
+    padding: '10px',
+    borderRadius: '10px',
+    //marginTop: 10,
+  },
+  section: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 15,
+    textAlign: 'center',
+    fontFamily: 'Helvetica',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 8,
+    marginBottom: 5,
+  },
+  textName: {
+    fontSize: 8,
+    marginBottom: 5,
+    fontFamily: 'Helvetica-Bold',
+  },
+  subtitle: {
+    fontSize: 8,
+    marginBottom: 5,
+  },
+  table: {
+    // display: "table",
+    width: 'auto',
+    marginBottom: 10,
+  },
+  tableRow: {
+    flexDirection: 'row',
+  },
+  tableCell: {
+    width: '25%',
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 5,
+    fontSize: 11,
+  },
+  cell: {
+    family: 'Helvetica',
+    width: '25%',
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 5,
+    fontSize: 11,
+    flexDirection: 'row',
+  },
+  image: { height: 60 },
+  imageEvent: {
+    width: 180,
+    height: '100%', // Ajusta a altura da imagem (ajuste conforme necessário)
+    objectFit: 'cover',
+  },
+  divider: {
+    marginTop: '10px',
+    borderTop: '2px dashed #bbb',
+    marginBottom: '20px',
+  },
+  footerText: {
+    fontSize: 10,
+  },
+  tabelaTitle: { fontFamily: 'Helvetica', fontSize: 12 },
 });
 Font.register({
-  family: 'Oswald',
-  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+  family: 'Helvetica',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf',
 });
-
+const UserRectangle = ({ user }: UserRectangleProps) => {
+  return (
+    <View style={styles.cell}>
+      <Image style={styles.image} src={(user as any).profilePhotoUrl} />
+      <View style={{ flexDirection: 'column' }}>
+        <Text style={styles.textName}>{(user as any).fullName}</Text>
+        <Text style={styles.text}>
+          Data Nasc: {new Date((user as any).birthday).toLocaleDateString()}
+        </Text>
+        <Text style={styles.text}>Email: {(user as any).email}</Text>
+        <Text style={styles.text}>Celular: {(user as any).cellphone}</Text>
+      </View>
+    </View>
+  );
+};
+const HeaderPdf = () => {
+  return (
+    <View style={styles.header}>
+      <Image style={styles.image} src={logoIccv} />
+      <Image style={styles.imageEvent} src={logoEvento} />
+      <Image style={styles.image} src={logoCursilho} />
+    </View>
+  );
+};
+const FooterPdf = ({ text }: FooterProps) => {
+  return (
+    <View style={styles.footer}>
+      <Text>{text}</Text>
+      <Text>{'Igreja de Cristo no Brasil'}</Text>
+    </View>
+  );
+};
 // Create Document Component
-function PdfEvent({ data }: PdfProps) {
+function PdfEvent({ data, textFooter }: PdfProps) {
   return (
     <Document>
       <Page orientation="landscape" style={styles.body}>
-            <View style={styles.header}> 
-              <Image style={styles.imageEvent} src={logoCursilho} />
-              <Text style={styles.title}>
-                4° CURSILHO FEMININO DE CRISTANDADE
-              </Text>  
-              <Image style={styles.image} src={logoIccv} />
+        <HeaderPdf />
+        <View style={styles.section}>
+          <View style={styles.table}>
+            <View style={styles.tableRow}>
+              {data.map((user) => (
+                <UserRectangle user={user} />
+              ))}
             </View>
-            <View style={styles.section}>
-              <View style={styles.table}>
-                <View style={styles.tableRow}>
-                  {data.map((user) => (
-                    <View style={styles.cell}>
-                      <View style={styles.image} />
-                      <View style={{ flexDirection: "column" }}>                      
-                        <Text style={styles.text}>Nome:{" "}{(user as any).fullName}</Text>
-                        <Text style={styles.text}>Data Nasc:{" "}{new Date((user as any).birthday).toLocaleDateString()}</Text>
-                        <Text style={styles.text}>Email:{" "}{(user as any).email}</Text>
-                        <Text style={styles.text}>Celular:{" "}{(user as any).cellphone}</Text>
-                      </View>
-                    </View>
-                  ))}
-                </View>
-              </View>
-            </View>
+          </View>
+        </View>
+        <FooterPdf text={textFooter} />
       </Page>
-  </Document>
+    </Document>
   );
 }
 
 export default PdfEvent;
-
-  
--- src/features/users/components/list.tsx
+++ src/features/users/components/list.tsx
@@ -4,14 +4,19 @@ import { useGetUsers } from '../api/getUsers';
 import { Button } from '@mui/material';
 import PdfEvent from '../../../components/pdfEvent';
 import FileSaver from 'file-saver';
-import { pdf } from '@react-pdf/renderer';
+import { PDFViewer, pdf } from '@react-pdf/renderer';
 import { formatDate, formatPhoneNumber } from '../../../utils';
 
 function List() {
-  const { data = [], isLoading } = useGetUsers();
+  //const { data = [], isLoading } = useGetUsers();
 
   async function handleDownloadPDF() {
-    const blob = await pdf(<PdfEvent data={data} />).toBlob();
+    const blob = await pdf(
+      <PdfEvent
+        data={users}
+        textFooter={'6° CURSILHO MASCULINO DE CRISTANDADE'}
+      />
+    ).toBlob();
     FileSaver.saveAs(blob, 'cursilho.pdf');
   }
 
@@ -33,23 +38,81 @@ function List() {
     { field: 'notes', headerName: 'Observações', flex: 1 },
   ];
   return (
-    <Card>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          handleDownloadPDF();
-        }}
-      >
-        Gerar PDF
-      </Button>
-      <DataGrid
-        rows={data}
-        autoHeight={true}
-        columns={columns}
-        loading={isLoading}
-      />
-    </Card>
+    <>
+      <Card>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            handleDownloadPDF();
+          }}
+        >
+          Gerar PDF
+        </Button>
+        <DataGrid
+          rows={users}
+          autoHeight={true}
+          columns={columns}
+          loading={false}
+        />
+      </Card>{' '}
+      <PDFViewer width={'100%'} height={'100%'}>
+        <PdfEvent
+          data={users}
+          textFooter={'28 de setembro a 01 de outubro de 2023'}
+        />
+      </PDFViewer>
+    </>
   );
 }
 
+const users = [
+  {
+    id: 5,
+    email: 'edudamaris33@gmail.com',
+    fullName: 'Eduarda Dâmaris Lima Diógenes',
+    cpf: '70025784480',
+    birthday: '2004-06-09T00:00:00.000Z',
+    cellphone: '84994669886',
+    worker: false,
+    diabetes: false,
+    hypertensive: false,
+    profession: 'Cursilhista',
+    city: 'Parnamirim',
+    state: 'RN',
+    role: 5,
+    profilePhotoUrl:
+      'https://firebasestorage.googleapis.com/v0/b/igrejadecristo-daed7.appspot.com/o/Screenshot from 2023-09-09 19-23-32.png?alt=media',
+    emergencyContact: null,
+    indicatedBy: null,
+    leadershipPosition: null,
+    religion: null,
+    notes: null,
+    createdAt: '2023-09-24T19:18:57.540Z',
+    updateAt: '2023-09-24T19:17:35.946Z',
+  },
+  {
+    id: 22,
+    email: '',
+    fullName: 'Ericka Karrolinne',
+    cpf: '15334822410',
+    birthday: '2004-03-22T00:00:00.000Z',
+    cellphone: '84999348788',
+    worker: false,
+    diabetes: false,
+    hypertensive: false,
+    profession: 'Cursilhista',
+    city: 'Natal',
+    state: 'RN',
+    role: 5,
+    profilePhotoUrl:
+      'https://firebasestorage.googleapis.com/v0/b/igrejadecristo-daed7.appspot.com/o/Screenshot from 2023-09-09 19-23-32.png?alt=media',
+    emergencyContact: '84987247979',
+    indicatedBy: null,
+    leadershipPosition: null,
+    religion: 'Evangelica',
+    notes: null,
+    createdAt: '2023-09-24T20:07:30.343Z',
+    updateAt: '2023-09-24T20:10:14.598Z',
+  },
+];
 export { List };
